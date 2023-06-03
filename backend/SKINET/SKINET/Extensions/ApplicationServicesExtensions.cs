402341df66b87fff8bd0ee9b882f1622459ca411
mina anwer <m.lewes@egyptianbanks.net>
2023-06-03 16:45:14 +0300

    refactor

---
+++ backend/SKINET/SKINET/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SKINET.Error;
+
+namespace SKINET.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection service , IConfiguration config)
+        {
+            service.AddControllers();
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+            service.AddDbContext<StoreContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            service.AddScoped<IProductRepo, ProductRepo>();
+            service.AddScoped(typeof(IGenericRepository<>), typeof(GenericRespository<>));
+            service.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            service.AddLogging(logging => logging.AddConsole());
+            service.Configure<ApiBehaviorOptions>(option =>
+            {
+                option.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+            return service;
+        }
+    }
+}
--- backend/SKINET/SKINET/Program.cs
+++ backend/SKINET/SKINET/Program.cs
@@ -1,50 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
-using Core.Interfaces;
-using AutoMapper;
-using Microsoft.Extensions.DependencyInjection;
 using SKINET.Middleware;
-using Microsoft.AspNetCore.Mvc;
-using SKINET.Error;
+using SKINET.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddScoped<IProductRepo, ProductRepo>();
-
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRespository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddLogging(logging => logging.AddConsole());
-builder.Services.Configure<ApiBehaviorOptions>(option =>
-{
-    option.InvalidModelStateResponseFactory = actionContext =>
-    {
-        var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0)
-        .SelectMany(x => x.Value.Errors)
-        .Select(x => x.ErrorMessage).ToArray();
-
-        var errorResponse = new ApiValidationErrorResponse
-        {
-            Errors = errors
-        };
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
-
-
-
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
+
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
  
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -60,6 +25,7 @@ using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
 var logger = services.GetService<ILogger>();
+
 try
 {
     await context.Database.MigrateAsync();
