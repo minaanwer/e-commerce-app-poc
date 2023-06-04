8c296589f7f2128dc7de564ab90147dc09ac05a9
mina anwer <m.lewes@egyptianbanks.net>
2023-06-04 20:51:00 +0300

    update

--- backend/SKINET/SKINET/Controllers/ProductsController.cs
+++ backend/SKINET/SKINET/Controllers/ProductsController.cs
@@ -32,10 +32,13 @@ namespace SKINET.Controllers
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
             var countSpec = new ProductWithFilterAndCountSpecification(productParams);
-            var totalItems = await _productRepo.CountAsync(spec);
+           
+            var totalItems = await _productRepo.CountAsync(countSpec);
             var products = await _productRepo.ListAsync(spec);
-            var data 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex ,productParams.PageSize,totalItems,data));
         }
 
         [HttpGet]
--- backend/SKINET/SKINET/Helpers/Pagination.cs
+++ backend/SKINET/SKINET/Helpers/Pagination.cs
@@ -2,6 +2,13 @@
 {
     public class Pagination<T> where T : class
     {
+        public Pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            Count = count;
+            Data = data;
+        }
 
         public int PageIndex { get; set; }
 
