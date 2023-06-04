ecac5dcda0709da332857273c8a025472b98728b
mina anwer <m.lewes@egyptianbanks.net>
2023-06-04 23:07:58 +0300

    update

--- frontend/client/src/app/app.component.ts
+++ frontend/client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'Client App';
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7190/api/Products').subscribe({
+      next: (response: any) => console.log(response),
+      error:(error: any) => console.log(error),
+      complete:()=>{
+        console.log("completed")
+      }
+    });
+
+  }
+
+
 }
--- frontend/client/src/app/app.module.ts
+++ frontend/client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
