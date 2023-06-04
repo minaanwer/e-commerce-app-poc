778d998d4a7796291b66209c55b84e1c29864776
mina anwer <m.lewes@egyptianbanks.net>
2023-06-04 22:12:43 +0300

    add nav bar component

--- frontend/client/src/app/app.module.ts
+++ frontend/client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
